066f7a3c830202a04f88c611254c7e202a287d5b
Rohan <rohan@algogenix.com>
2023-12-10 00:39:36 -0600

    new updates

--- src/app/pages/user/help/help.page.ts
+++ src/app/pages/user/help/help.page.ts
@@ -6,17 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.page.scss'],
 })
 export class HelpPage implements OnInit {
-
-  helpOptions(){
+  helpOptions() {
     return [
-      {name: 'upscaling guide', url:'assets/UI_Images/Regular_Size/App_UI_Buttons_small_size-assets/User_Info.png'},
-      {name: 'faq', url:'assets/UI_Images/Regular_Size/App_UI_Buttons_small_size-assets/User_Info.png'}
-    ]
+      {
+        name: 'upscaling guide',
+        url: 'assets/UI_Images/WebP_Regular_Size/App_UI_Buttons_small_size-assets/User_Info.webp',
+      },
+      {
+        name: 'faq',
+        url: 'assets/UI_Images/WebP_Regular_Size/App_UI_Buttons_small_size-assets/User_Info.webp',
+      },
+    ];
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
